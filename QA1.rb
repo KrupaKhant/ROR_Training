c556b9aadeb7ea8e425cca8401b294df8be06c05
Krupa <krupa.khant@bacancy.com>
2023-02-23 10:13:13 +0530

    Assignment 5 completed

---
+++ QA1.rb
@@ -0,0 +1,18 @@
+require 'date'
+
+
+birthday = DateTime.new(2000,2,2,0,0,0)
+
+
+puts "Current time is:" + Time.now.to_s
+puts"Birthday is: #{ birthday.to_time}"
+
+seconds = Time.now - birthday.to_time
+minutes = seconds / 60
+hours = minutes / 60
+days = hours / 24
+months = days / 30.44
+years = months / 12
+
+
+puts "Age is #{years.to_i} year #{(months % 12).to_i} month #{(days % 30.44).to_i} days #{(hours % 24).to_i} hours and #{(minutes % 60).to_i} minutes"
